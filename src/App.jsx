6cb92bb986b07304d187fa47404ff16e963720a5
GayaN948 <gayanudayanga1221@gmail.com>
2026-01-23 09:56:56 +0530

    Button Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Organisms/header'
 import Footer from './components/Organisms/footer'
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
+       <main className='content'>
+        <Home/>
+       </main>
       <Footer />
     </div>
   );
