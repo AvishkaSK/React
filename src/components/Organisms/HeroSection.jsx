956c8ec757c303f3d22b92df60217e76db32e9f5
GayaN948 <gayanudayanga1221@gmail.com>
2026-01-23 10:27:35 +0530

    Hero Section begin

---
+++ src/components/Organisms/HeroSection.jsx
@@ -0,0 +1,17 @@
+import SportImage from '../../assets/images/banner.png'
+import Button from '../atoms/button';
+
+const HeroSection = () =>{
+    return(
+        <div className='position-relative overflow-hiddin' style={{background:'linear-gradient(135deg, #667eea 0% , #764ba2 100%)',minHeight:'400px'}}>
+            <div className='container'>
+                <div className='row align-items-center min-vh-50'>
+                    <div className='col-lg-6 text-white py-5'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default HeroSection;
